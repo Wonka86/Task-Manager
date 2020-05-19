0bfd2f950a58358f53a2fc26268741d04369dd32
Jude <storeyjude@hotmail.com>
2020-05-19 10:22:07 +0000

    created add task template

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
@@ -13,6 +13,11 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+@app.route('/')
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
