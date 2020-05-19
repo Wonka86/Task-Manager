0236096d8cdaddc68d16bc8e7f02b2466577c6c5
Jude <storeyjude@hotmail.com>
2020-05-19 12:15:47 +0000

    fixed error

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ def get_tasks():
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html', categories=mongo.db.categories.find())
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
